8fb2fdc7cf852665d84ddfa66bc23b1fcd45d311
Jason Mendoza <mernstackdev729@gmail.com>
2023-03-30 09:34:31 +0900

    update: db config

--- config.php
+++ config.php
@@ -23,9 +23,9 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'little_mini_opencart');
+define('DB_HOSTNAME', '92.205.3.167');
+define('DB_USERNAME', 'littlemini');
+define('DB_PASSWORD', 'GhMyhk&bCVjpMk@d');
+define('DB_DATABASE', 'littlemini');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
