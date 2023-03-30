1d8da1c73d433eb6b4e3525a0dcc309a6afd95c6
Jason Mendoza <mernstackdev729@gmail.com>
2023-03-30 10:06:46 +0900

    checkpoint

--- admin/config.php
+++ admin/config.php
@@ -1,12 +1,8 @@
 <?php
-// APPLICATION
+// ========== DEV ========== 
 define('APPLICATION', 'Admin');
-
-// HTTP
 define('HTTP_SERVER', 'http://localhost/admin/');
 define('HTTP_CATALOG', 'http://localhost/');
-
-// DIR
 define('DIR_OPENCART', 'E:/xampp/htdocs/');
 define('DIR_APPLICATION', DIR_OPENCART . 'admin/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
@@ -22,15 +18,39 @@ define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
 define('DIR_LOGS', DIR_STORAGE . 'logs/');
 define('DIR_SESSION', DIR_STORAGE . 'session/');
 define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
-
-// DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', '92.205.3.167');
-define('DB_USERNAME', 'littlemini');
-define('DB_PASSWORD', 'GhMyhk&bCVjpMk@d');
-define('DB_DATABASE', 'littlemini');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'little_mini_opencart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
-
-// OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
+
+// ========== PROD ========== 
+// define('APPLICATION', 'Admin');
+// define('HTTP_SERVER', 'https://boutiquemignonne.fr/admin/');
+// define('HTTP_CATALOG', 'https://boutiquemignonne.fr/');
+// define('DIR_OPENCART', '/home/xttl3xy3mfcj/public_html/');
+// define('DIR_APPLICATION', DIR_OPENCART . 'admin/');
+// define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+// define('DIR_IMAGE', DIR_OPENCART . 'image/');
+// define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+// define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+// define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+// define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+// define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+// define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+// define('DIR_CACHE', DIR_STORAGE . 'cache/');
+// define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+// define('DIR_LOGS', DIR_STORAGE . 'logs/');
+// define('DIR_SESSION', DIR_STORAGE . 'session/');
+// define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', '92.205.3.167');
+// define('DB_USERNAME', 'littlemini');
+// define('DB_PASSWORD', 'GhMyhk&bCVjpMk@d');
+// define('DB_DATABASE', 'littlemini');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
+// define('OPENCART_SERVER', 'https://www.opencart.com/');
