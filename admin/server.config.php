9ef68a3865d6e8945ba92a636516a0fde134537b
Jason Mendoza <mernstackdev729@gmail.com>
2023-03-31 06:37:34 +0900

    add:server.config

---
+++ admin/server.config.php
@@ -0,0 +1,28 @@
+<?php
+
+define('APPLICATION', 'Admin');
+define('HTTP_SERVER', 'https://boutiquemignonne.fr/admin/');
+define('HTTP_CATALOG', 'https://boutiquemignonne.fr/');
+define('DIR_OPENCART', '/home/xttl3xy3mfcj/public_html/');
+define('DIR_APPLICATION', DIR_OPENCART . 'admin/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'littlemini');
+define('DB_PASSWORD', 'GhMyhk&bCVjpMk@d');
+define('DB_DATABASE', 'littlemini');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+define('OPENCART_SERVER', 'https://www.opencart.com/');
